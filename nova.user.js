1f5b9f37cabddec04bfb91de4753b7c2a2117bcc
Eirik Hanssen <eirikhanssen79@gmail.com>
2016-09-05 10:32:23 +0200

    poll for changes using setinterval

--- nova.user.js
+++ nova.user.js
@@ -728,17 +728,30 @@ var alle_serier = {
 		var temahefteLenker = alle_serier.temahefte;
 
 	function openPages(pageLinks){
-		console.log("already stored?");
-		console.log(GM_getValue(pageLinks[0]));
+		//console.log("already stored?");
+		//console.log(GM_getValue(pageLinks[0]));
 		console.log("opening window: " + pageLinks[0]);
-		var win = window.open(pageLinks[0]);
+		var currentLink = pageLinks[0]
+        console.log("opening: " + currentLink);
+		var win = window.open(currentLink);
+        
+		var checkIfWinStillOpenLoop = setInterval(function(){
+            if(!win.closed) {
+				console.log("still open");
+                
+			} else {
+                clearInterval(checkIfWinStillOpenLoop);
+				console.log("the popup-page has closed: " + currentLink);
+            }
+		}, 2);
+        
 		/*win.addEventListener('unload', function(event) {
         	console.log("Window closed: " + pageLinks[0]);
         	console.log("Data collected for this url: ");
         	console.log(GM_getValue(pageLinks[0]));
       	});*/
 
-      	win.onunload = function() {
+      	/*win.onunload = function() {
   		if (window.opener && typeof(window.opener.onPopupClosed) == 'function') {
     		window.opener.onPopupClosed();
     		console.log("Window unloaded: " + pageLinks[0]);
@@ -747,7 +760,7 @@ var alle_serier = {
 
 		window.onPopupClosed = function() {
   			console.log("You closed the pop up: " +  pageLinks[0]);
-		};
+		};*/
 
 	} // openPages(); 
 
